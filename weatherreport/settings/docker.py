97ed2e2f53b03b93a213c1fd76e83671e22f370c
Sarah Boyce <sarah.boyce@quickrelease.de>
2021-01-28 19:28:24 +0100

    A/ django project and settings files for local and docker

---
+++ weatherreport/settings/docker.py
@@ -0,0 +1,39 @@
+import os
+from weatherreport.settings.common import *
+
+DEBUG = int(os.environ.get("DEBUG", default=0))
+
+STATIC_ROOT = os.path.join(SITE_ROOT, "staticfiles")
+
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+
+TEMPLATES[0]["OPTIONS"]["debug"] = DEBUG
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": os.environ.get("POSTGRES_PORT"),
+    }
+}
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.environ.get("REDIS_URL"),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "CONNECTION_POOL_KWARGS": {"max_connections": 30},
+        },
+        "KEY_PREFIX": "prod",
+    }
+}
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+
+MEDIA_ROOT = "/usr/src/app/weatherreport/mediafiles/"
+
+MEDIA_DIRECTORY = "/media/"
---
+++ weatherreport/settings/local.py
@@ -0,0 +1,36 @@
+"""
+Django local settings for weatherreport project.
+
+This file contains settings unsuitable for production
+Please see production.py as an example file for a production deployment
+"""
+from .common import *
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+
+TEMPLATES[0]["OPTIONS"]["debug"] = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "weatherreport",
+        "USER": "postgres",
+        "PASSWORD": "a",
+        "HOST": "localhost",
+        "PORT": "5432",
+    },
+}
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.db.DatabaseCache",
+        "LOCATION": "cachetable",
+        "KEY_PREFIX": "local",
+    }
+}
